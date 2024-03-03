9ee696e879b41b94e7ff168cb86eeb6e5761ce6e
Тимохин Семён Сергеевич <sesetimokhin@edu.hse.ru>
2024-03-03 15:30:45 +0300

    SECOND DONE

--- main.py
+++ main.py
@@ -29,28 +29,6 @@ def task1():
 
     return common_len - (need_max - need_min + 1)
 
-def task2():
-    str1 = input()
-    str2 = input()
-    is_first_home = True if input() == "1" else False
-    fst_match = list(map(int, str1.split(":")))
-    snd_match = list(map(int, str2.split(":")))
-    need_goals = fst_match[1] + snd_match[1] - fst_match[0] - snd_match[0]
-    if is_first_home:
-        if snd_match[1] < snd_match[0] + need_goals:
-            return (need_goals)
-        else:
-            return (need_goals + 1)
-    else:
-        if fst_match[0] > fst_match[1]:
-            advantage = True
-        else:
-            advantage = False
-        if advantage:
-            return need_goals
-        else:
-            return (need_goals + 1)
-
 
 def task3():
     def calc_quantity(number):
@@ -76,6 +54,73 @@ def task3():
         res += calc_quantity(int(input()))
     return res
 
+
+def task2():
+    class Team:
+        def __init__(self, first_score, second_score, is_first_home, is_second_home):
+            self.first_score = first_score
+            self.second_score = second_score
+            self.is_first_home = is_first_home
+            self.is_second_home = is_second_home
+            self.advantage = False
+
+        def get_first_score(self):
+            return self.first_score
+
+        def get_second_score(self):
+            return self.second_score
+
+        def get_is_first_home(self):
+            return self.is_first_home
+
+        def get_is_second_home(self):
+            return self.is_second_home
+
+        def get_advantage(self):
+            return self.advantage
+
+        def set_second_score(self, number):
+            self.second_score = number
+
+        def set_advatage(self, advantage):
+            self.advantage = advantage
+
+    def check_for_advantage(team1, team2):
+        if team1.get_is_first_home():  # second game in guests
+            if team1.get_second_score() > team2.get_first_score():
+                team1.set_advatage(True)
+            if team1.get_second_score() < team2.get_first_score():
+                team2.set_advatage(True)
+        else:  # first game in guests
+            if team1.get_first_score() > team2.get_second_score():
+                team1.set_advatage(True)
+            if team1.get_first_score() < team2.get_second_score():
+                team2.set_advatage(True)
+
+    str1 = input()
+    str2 = input()
+    if int(input()) == 1:
+        fst_home = True
+    else:
+        fst_home = False
+
+    team1 = Team(int(str1.split(':')[0]), int(str2.split(':')[0]), fst_home, not fst_home)
+    team2 = Team(int(str1.split(':')[1]), int(str2.split(':')[1]), not fst_home, fst_home)
+
+    common_score_first_team = team1.first_score + team1.second_score
+    common_score_second_team = team2.first_score + team2.second_score
+    difference = common_score_second_team - common_score_first_team
+
+    team1.second_score = team1.second_score + difference
+
+    check_for_advantage(team1, team2)
+
+    if team1.get_advantage():
+        return difference if difference >= 0 else 0
+    else:
+        return difference + 1 if difference >= 0 else 0
+
+print(task2())
 
 def task7():
     my_sold = int(input())
@@ -116,5 +161,44 @@ def task7():
                 return -1
 
 
+def task4():
+    global rem_r, rem_b
+    board = []
+    rem_b_ex = False
+    rem_r_ex = False
+    for line in range(8):
+        board.append([])
+        string = input()
+        for col in range(8):
+            cur_symb = string[col]
+            if cur_symb == 'R':
+                board[line].append('R')
+                rem_r = [line, col]
+                rem_r_ex = True
+            if cur_symb == 'B':
+                board[line].append('B')
+                rem_b = [line, col]
+                rem_b_ex = True
+            else:
+                board[line].append('*')
+    k = 0
+    for line in range(8):
+        for col in range(8):
+            if rem_r_ex and line == rem_r[0]:
+                board[line][col] = 'R'
+            if rem_r_ex and col == rem_r[1]:
+                board[line][col] = 'R'
+            if rem_b_ex and abs(line - rem_b[0]) == abs(col - rem_b[1]):
+                board[line][col] = 'B'
+    for i in range(8):
+        for j in range(8):
+            print(board[i][j], end='')
+        print()
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] == '*':
+                k += 1
+    return k
 
-print(task7())
+
+
